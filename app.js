6d5d741e4883ae44e27542cbba47a69a90f07118
aakash <nxtcoder.aakashkumar@gmail.com>
2023-03-10 17:08:51 +0000

    cors update

--- app.js
+++ app.js
@@ -7,27 +7,30 @@ let sqlite3 = require("sqlite3");
 
 let app = express();
 const cors = require("cors");
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+  })
+);
 app.use(express.json());
-
 
 let dbPath = path.join(__dirname, "studentMaster.db");
 let db = null;
 
 let initializeDbAndServer = async () => {
-    try {
-        db = await open({
-            filename: dbPath,
-            driver: sqlite3.Database,
-        });
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
 
-        app.listen(3005, () => {
-            console.log("The Server is running at http://localhost:3005/");
-        });
-    } catch (e) {
-        console.log(`The Error is : ${e.message}`);
-        process.exit(1);
-    }
+    app.listen(3005, () => {
+      console.log("The Server is running at http://localhost:3005/");
+    });
+  } catch (e) {
+    console.log(`The Error is : ${e.message}`);
+    process.exit(1);
+  }
 };
 
 initializeDbAndServer();
@@ -35,11 +38,11 @@ initializeDbAndServer();
 //API 1
 
 app.post("/register", async (request, response) => {
-    try {
-        let { name, email, password, mobile, isStudent } = request.body;
-        let hashedPassword = await bcrypt.hash(request.body.password, 10);
+  try {
+    let { name, email, password, mobile, isStudent } = request.body;
+    let hashedPassword = await bcrypt.hash(request.body.password, 10);
 
-        let getUserQuery = `
+    let getUserQuery = `
     SELECT 
         * 
     FROM 
@@ -48,18 +51,18 @@ app.post("/register", async (request, response) => {
         email='${email}';
     `;
 
-        let userDetails = await db.get(getUserQuery);
+    let userDetails = await db.get(getUserQuery);
 
-        if (userDetails !== undefined) {
-            response.status(400);
-            response.send("User already exists");
-        } else {
-            if (password.length < 5) {
-                response.status("400");
-                response.send("Password is too short");
-            } else {
-                let hashedPassword = await bcrypt.hash(password, 10);
-                let createUserQuery = `
+    if (userDetails !== undefined) {
+      response.status(400);
+      response.send("User already exists");
+    } else {
+      if (password.length < 5) {
+        response.status("400");
+        response.send("Password is too short");
+      } else {
+        let hashedPassword = await bcrypt.hash(password, 10);
+        let createUserQuery = `
                 INSERT INTO 
                     studentMaster(name,email,password,mobile,isStudent) 
                      
@@ -68,23 +71,23 @@ app.post("/register", async (request, response) => {
                 );
                 `;
 
-                await db.run(createUserQuery);
-                response.status(200);
-                response.send("User created successfully");
-            }
-        }
-    } catch (e) {
-        console.log(`The Error is : ${e.message}`);
-        process.exit(1);
+        await db.run(createUserQuery);
+        response.status(200);
+        response.send("User created successfully");
+      }
     }
+  } catch (e) {
+    console.log(`The Error is : ${e.message}`);
+    process.exit(1);
+  }
 });
 
 // API 2
 
 app.post("/login", async (request, response) => {
-    try {
-        let { email, password } = request.body;
-        let getUserQuery = `
+  try {
+    let { email, password } = request.body;
+    let getUserQuery = `
         SELECT 
             * 
         FROM 
@@ -93,33 +96,33 @@ app.post("/login", async (request, response) => {
             email='${email}';
         `;
 
-        let dbUser = await db.get(getUserQuery);
+    let dbUser = await db.get(getUserQuery);
 
-        if (dbUser === undefined) {
-            response.status(400);
-            response.send("Invalid user");
-        } else {
-            let isPasswordCorrect = await bcrypt.compare(password, dbUser.password);
-            if (isPasswordCorrect) {
-                response.status(200);
-                response.send({ name: dbUser.name, isStudent: dbUser.isStudent });
-            } else {
-                response.status(400);
-                response.send("Invalid password");
-            }
-        }
-    } catch (e) {
-        console.log(`The Error is : ${e.message}`);
-        process.exit(1);
+    if (dbUser === undefined) {
+      response.status(400);
+      response.send("Invalid user");
+    } else {
+      let isPasswordCorrect = await bcrypt.compare(password, dbUser.password);
+      if (isPasswordCorrect) {
+        response.status(200);
+        response.send({ name: dbUser.name, isStudent: dbUser.isStudent });
+      } else {
+        response.status(400);
+        response.send("Invalid password");
+      }
     }
+  } catch (e) {
+    console.log(`The Error is : ${e.message}`);
+    process.exit(1);
+  }
 });
 
 //API 3
 
 app.put("/change-password", async (request, response) => {
-    try {
-        let { email, oldPassword, newPassword } = request.body;
-        let getUserDetailsQuery = `
+  try {
+    let { email, oldPassword, newPassword } = request.body;
+    let getUserDetailsQuery = `
     SELECT 
         * 
     FROM 
@@ -127,20 +130,20 @@ app.put("/change-password", async (request, response) => {
     WHERE 
         email='${email}';`;
 
-        let dbDetail = await db.get(getUserDetailsQuery);
+    let dbDetail = await db.get(getUserDetailsQuery);
 
-        let isPasswordCorrect = await bcrypt.compare(
-            oldPassword,
-            dbDetail.password
-        );
+    let isPasswordCorrect = await bcrypt.compare(
+      oldPassword,
+      dbDetail.password
+    );
 
-        if (isPasswordCorrect) {
-            if (newPassword.length < 5) {
-                response.status(400);
-                response.send("Password is too short");
-            } else {
-                let hashedPassword = await bcrypt.hash(newPassword, 10);
-                let updatePasswordQuery = `
+    if (isPasswordCorrect) {
+      if (newPassword.length < 5) {
+        response.status(400);
+        response.send("Password is too short");
+      } else {
+        let hashedPassword = await bcrypt.hash(newPassword, 10);
+        let updatePasswordQuery = `
                 UPDATE  
                     user 
                 SET 
@@ -148,18 +151,18 @@ app.put("/change-password", async (request, response) => {
                 WHERE 
                     username='${username}';
                 `;
-                await db.run(updatePasswordQuery);
-                response.status(200);
-                response.send("Password updated");
-            }
-        } else {
-            response.status(400);
-            response.send("Invalid current password");
-        }
-    } catch (e) {
-        console.log(`The Error is : ${e.message}`);
-        process.exit(1);
+        await db.run(updatePasswordQuery);
+        response.status(200);
+        response.send("Password updated");
+      }
+    } else {
+      response.status(400);
+      response.send("Invalid current password");
     }
+  } catch (e) {
+    console.log(`The Error is : ${e.message}`);
+    process.exit(1);
+  }
 });
 
 module.exports = app;
@@ -167,29 +170,29 @@ module.exports = app;
 //API 5
 
 app.get("/allusers", async (request, response) => {
-    try {
-        const getUserQuery = `
+  try {
+    const getUserQuery = `
     SELECT 
         * 
     FROM 
         studentMaster 
     `;
 
-        let userDetails = await db.all(getUserQuery);
-        response.send(userDetails);
-    } catch (e) {
-        console.log(`The Error is : ${e.message}`);
-        process.exit(1);
-    }
+    let userDetails = await db.all(getUserQuery);
+    response.send(userDetails);
+  } catch (e) {
+    console.log(`The Error is : ${e.message}`);
+    process.exit(1);
+  }
 });
 
 //API 4
 
 app.get("/:email/", async (request, response) => {
-    try {
-        let { email } = request.params;
+  try {
+    let { email } = request.params;
 
-        let getUserQuery = `
+    let getUserQuery = `
     SELECT 
         * 
     FROM 
@@ -198,14 +201,14 @@ app.get("/:email/", async (request, response) => {
         email='${email}';
     `;
 
-        let userDetails = await db.get(getUserQuery);
-        if (userDetails) {
-            response.send(userDetails);
-        }
-        response.send("No Such User Exists");
-        response.send(userDetails);
-    } catch (e) {
-        console.log(`The Error is : ${e.message}`);
-        process.exit(1);
+    let userDetails = await db.get(getUserQuery);
+    if (userDetails) {
+      response.send(userDetails);
     }
+    response.send("No Such User Exists");
+    response.send(userDetails);
+  } catch (e) {
+    console.log(`The Error is : ${e.message}`);
+    process.exit(1);
+  }
 });
